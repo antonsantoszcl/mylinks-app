0e8c4154f9e6d304eff09ad93d215f4c0f79d7bd
antonsantoszcl <antonsantoszcl@gmail.com>
2026-06-04 13:46:49 -0300

    feat: replace instrucoes video with new version (local file)

--- src/components/layout/TopNav.tsx
+++ src/components/layout/TopNav.tsx
@@ -49,7 +49,7 @@ function VideoOverlay({ open, onClose }: { open: boolean; onClose: () => void })
         </button>
         <video
           id="instrucoes-overlay-video"
-          src="https://drive.google.com/uc?export=download&id=178AU8xoHXevBDbTLnYzxjXcsXRJpbb0C"
+          src="/videos/instrucoes.mp4"
           style={{ width: '100%', height: '100%', objectFit: 'contain' }}
           autoPlay
           loop
